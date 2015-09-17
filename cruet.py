9b9d8d8eda1f8d24f5c09402d1b7a0d02e0e99f6
Tomasz Bukowski <tomasz.bukowski@cert.pl>
2015-09-17 14:56:30 +0200

    code1

--- cruet.py
+++ cruet.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+ #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 #
@@ -11,19 +11,77 @@ __license__ = 'MIT'
 #
 #
 
-
-
 def route(path):
   print path
   def x(a):
     print a
   return x
 
-def standalone(): # parse commandline args, run()
-  pass
+class GenericServer(object):
+  def __init__(self, host, port, **options):
+    print "__init__"
+    self.host = host
+    self.port = int(port)
+    self.server = None
+    self.options = options
 
-def run(host='0.0.0.0', port=12345):
+  def run(self, wsgi_app):
+    pass
+
+
+class WSGIRefServer(GenericServer):
+
+  def run(self, wsgi_app):
+    import wsgiref.simple_server as ref_srv
+    class FixedHandler(ref_srv.WSGIRequestHandler):
+      def address_string(self):  # Prevent reverse DNS lookups please.
+        return self.client_address[0]
+
+      def x_log_request(*args, **kw):
+        if not self.quiet:
+          return WSGIRequestHandler.log_request(*args, **kw)
+
+    srv_class = ref_srv.WSGIServer
+    hnd_class = FixedHandler
+    self.server = ref_srv.make_server(self.host, self.port, wsgi_app, srv_class, hnd_class)
+    try:
+      self.server.serve_forever()
+    except KeyboardInterrupt:
+      self.server.server_close()
+
+
+def wsgi_handler(environ, start_response):
+  print "WSGI handler ..."
+  status = '200 OK'
+  headers = []
+  exc_info = None
+  writer = start_response(status, headers, exc_info)
+  writer('test1')
+  print writer
+  return 'test2'
+
+
+application = wsgi_handler
+
+
+def run(host='0.0.0.0', port=12345, server_class=None, **opts):
+  if server_class is None:
+    server_class = WSGIRefServer
   print "Run"
+  handle = server_class(host, port, **opts)
+  handle.run(wsgi_handler)
 
-def wsgi():
-  pass
+
+
+if __name__ == '__main__':
+  run()
+
+# end 
+
+
+
+
+
+
+
+
