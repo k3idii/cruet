b3310332034a3b238f1c496907d7a3740d2e466e
KD <keidii@gmail.com>
2015-09-18 02:11:49 +0200

    local

--- cruet.py
+++ cruet.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 #
@@ -7,73 +7,14 @@ __author__ = 'KeiDii'
 __version__ = '0.1'
 __license__ = 'MIT'
 
-#
-#
-#
 
-<<<<<<< HEAD
 DEFAULT_LISTEN_HOST = '0.0.0.0'
 DEFAULT_LISTEN_PORT = 8008
 
-class WSGIApp(object:
-  auto_handle = False
 
-  def __init_(self, env, start_response):
-    self.env = env
-    self.start_response = start_response
-    if self.auto_handle:
-      self.handle()
-
-  def handle(self):
-    print "Handle !"
-
-
-=======
->>>>>>> 9b9d8d8eda1f8d24f5c09402d1b7a0d02e0e99f6
-def route(path):
-  print path
-  def x(a):
-    print a
-  return x
-
-<<<<<<< HEAD
-
-class StandaloneServer(Object):
-
-  srv = None
-
-  def __init__(self, options, app):
-    self.options = options
-    self.app = app
-
-  def run(self):
-    pass
-
-
-class WSGIRefServer(StandaloneServer):
-  def run(self):
-    import wsgiref.simple_server
-    host = self.options.get('host', DEFAULT_LISTEN_HOST)
-    port = self.options.get('port', DEFAUTT_LISTEN_PORT)
-    srv_class = wsgiref.simple_server.WSGIServer
-    hdn_class = wsgiref.simple_server.WSGIRequestHandler
-    self.srv = wsgiref.simple_server.make_server(host, port, self.app, srv_class, hdn_class)
-    try:
-      self.srv.serve_forever()
-    except KeyboardInterrupt:
-      self.srv.server_close()
-      raise
-
-
-
-def standalone(): # parse commandline args, run()
-  pass
-=======
 class GenericServer(object):
-  def __init__(self, host, port, **options):
+  def __init__(self, **options):
     print "__init__"
-    self.host = host
-    self.port = int(port)
     self.server = None
     self.options = options
 
@@ -82,59 +23,93 @@ class GenericServer(object):
 
 
 class WSGIRefServer(GenericServer):
-
   def run(self, wsgi_app):
+
+    host = self.options.get('host', DEFAULT_LISTEN_HOST)
+    port = int(self.options.get('port', DEFAULT_LISTEN_PORT))
+
     import wsgiref.simple_server as ref_srv
+
     class FixedHandler(ref_srv.WSGIRequestHandler):
       def address_string(self):  # Prevent reverse DNS lookups please.
         return self.client_address[0]
 
-      def x_log_request(*args, **kw):
-        if not self.quiet:
-          return WSGIRequestHandler.log_request(*args, **kw)
+      def log_request(*args, **kw):
+        pass
 
     srv_class = ref_srv.WSGIServer
     hnd_class = FixedHandler
-    self.server = ref_srv.make_server(self.host, self.port, wsgi_app, srv_class, hnd_class)
+    print "Starting WSGIRef simple server on {0}:{1}".format(host,port)
+    self.server = ref_srv.make_server(host, port, wsgi_app, srv_class, hnd_class)
     try:
       self.server.serve_forever()
     except KeyboardInterrupt:
       self.server.server_close()
 
 
-def wsgi_handler(environ, start_response):
-  print "WSGI handler ..."
-  status = '200 OK'
-  headers = []
-  exc_info = None
-  writer = start_response(status, headers, exc_info)
-  writer('test1')
-  print writer
-  return 'test2'
 
 
-application = wsgi_handler
->>>>>>> 9b9d8d8eda1f8d24f5c09402d1b7a0d02e0e99f6
+class CIDict(dict): # Case Insensitive Dict
+  pass
+
+class RequestRouter(object):
+  pass
 
 
-def run(host='0.0.0.0', port=12345, server_class=None, **opts):
+class Cruet(object):
+
+  _write_using_writer = False
+
+  def __init__(self):
+    pass
+
+  def route(self, *a, **kw):
+    def wrapper(f):
+      print "Wrapped :", f
+
+    print a, kw
+    return wrapper
+
+  def handle_request(self):
+    pass
+
+  def wsgi_handler(self, environ, start_response):
+
+    status = '200 OK'
+    headers = []
+    exc_info = None
+    body_writer = start_response(status, headers, exc_info)
+
+    body = "<html> it is"
+
+    if self._write_using_writer:
+      body_writer(body)
+      return ''
+    else:
+      return body
+
+
+def run(server_class=None, **opts):
   if server_class is None:
     server_class = WSGIRefServer
-  print "Run"
-  handle = server_class(host, port, **opts)
+  handle = server_class(**opts)
   handle.run(wsgi_handler)
 
+def wsgi_handler(environ, start_response):
+  print "External WSGI handler ..."
+  # forward request to global scope Cruet instance
+  return cr.wsgi_handler(environ, start_response)
+
+cr = Cruet()
+
+# expose functions :
+route = cr.route
+
+# expose WSGI handler
+application = wsgi_handler
 
 
 if __name__ == '__main__':
   run()
 
-# end 
-
-
-
-
-
-
-
-
+# end
